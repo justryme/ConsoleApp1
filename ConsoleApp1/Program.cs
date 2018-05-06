caa3cf13cb22d427fd4063a8d24c1ac84cdb19b9
john smith <justryme@yahoo.com>
2018-05-06 15:21:12 -0400

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo input;
+            
+            do
+            {                
+                Console.WriteLine("Enter a number:");
+                input = Console.ReadKey();
+                Console.WriteLine("\r");
+            }
+            while (input.Key != ConsoleKey.D1);
+            
+
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> results = new List<IList<int>>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    for (int k = j + 1; k < nums.Length; k++)
+                    {
+                        if (nums[i] + nums[j] + nums[k] == 0)                            
+                            results.Add(new List<int> { nums[i], nums[j], nums[k] });                        
+                    }                       
+                }
+            }
+
+            results.Distinct();
+            if (results.Count > 0)
+                return results;
+            else
+                throw new Exception("no three sum solution");
+        }
+    }
+}
